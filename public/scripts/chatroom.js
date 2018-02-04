598fb21134bfd6ba621e578042cc66aff1bab35d
Marco Beltempo <marco.beltempo@gmail.com>
2018-02-04 16:04:27 -0500

    Added Travis CI, live development refresh, jslint+ prettier

--- public/scripts/chatroom.js
+++ public/scripts/chatroom.js
@@ -1,106 +1,126 @@
 var user = null;
-$(function () {
-    var socket = io.connect(document.location.origin);
-    socket.on('news', function (data) {
-        console.log(data);
-    });
+$(function() {
+  var socket = io.connect(document.location.origin);
+  socket.on("news", function(data) {
+    console.log(data);
+  });
 
-    //check weather username is stored in the session, if yes then directly authenticate the user
-    if(sessionStorage.username != undefined) {
-        $('#username').hide(sessionStorage.username);
-        $('.brand').html();
-        $('.brand').show();
-        $("#message").show();
-        $("#message").focus();
-        socket.emit('adduser', sessionStorage.username);
+  //check weather username is stored in the session, if yes then directly authenticate the user
+  if (sessionStorage.username != undefined) {
+    $("#username").hide(sessionStorage.username);
+    $(".brand").html();
+    $(".brand").show();
+    $("#message").show();
+    $("#message").focus();
+    socket.emit("adduser", sessionStorage.username);
+  } else {
+    //hiding the brand
+    $(".brand").hide();
+    $("#message").hide();
+    $("#username").focus();
+  }
+
+  $("#send").click(function() {
+    console.log("Send pressed");
+    //check if username is null if yes then fire addUsername event and send the username
+    //handle message send
+    var msg = $("#message").val();
+    if (msg != "") {
+      var safe = msg
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;");
+      console.log("message is not null");
+      socket.emit("message", {
+        message: safe,
+        user: sessionStorage.username,
+        timestamp: new Date()
+      });
     }
-    else {
-        //hiding the brand
-        $('.brand').hide();
-        $('#message').hide();
-        $("#username").focus();
-    }	
+    document.getElementById("message").value = "";
+  });
 
-    $("#send").click(function() {
-        console.log("Send pressed");
-        //check if username is null if yes then fire addUsername event and send the username
-        //handle message send
-        var msg = $('#message').val();
-        if(msg != ""){
-            var safe = msg.replace(/&/g, '&amp;').replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-            console.log("message is not null");
-            socket.emit('message', {message: safe, user: sessionStorage.username, timestamp: new Date()}); 
-        }
-        document.getElementById("message").value = "";        
-    });
+  //handle enter key event on messagebox
+  $("#message").keydown(function(e) {
+    if (e.which === 13) {
+      //handle message send
+      var msg = $("#message").val();
+      $("#message").val("");
+      e.preventDefault();
+      if (msg != "") {
+        var safe = msg
+          .replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;")
+          .replace(/"/g, "&quot;");
+        console.log("message is not null");
+        socket.emit("message", {
+          message: safe,
+          user: user,
+          timestamp: new Date()
+        });
+      }
+      document.getElementById("message").value = "";
+    }
+  });
 
-    
-    //handle enter key event on messagebox
-    $("#message").keydown(function (e) {
-        if(e.which === 13){
-            //handle message send
-            var msg = $('#message').val();
-            $('#message').val('');
-            e.preventDefault();
-            if(msg != ""){
-                var safe = msg.replace(/&/g, '&amp;').replace(/</g, '&lt;')
-                .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-                console.log("message is not null");
-                socket.emit('message', {message: safe, user: user, timestamp: new Date()}); 
-            }
-            document.getElementById("message").value = "";            
-        }
-    });
+  socket.on("userAdded", function(data) {
+    //add banner containing the username
+    $("#username").hide();
+    $(".brand").html(data.username);
+    $(".brand").show();
+    $("#message").show();
+    $("#message").focus();
+    user = data.username;
+  });
 
-    socket.on('userAdded', function(data) {
-        //add banner containing the username
-        $('#username').hide();
-        $('.brand').html(data.username);
-        $('.brand').show();
-        $("#message").show();
-        $("#message").focus();
-        user = data.username;
-    });
+  socket.on("updateUsers", function(data) {
+    if (data != null) {
+      $("#onlineUserList").html("");
+      for (var key in data.usernames) {
+        //add the user to the list of online users
+        $("#onlineUserList").append("<li>" + key + "</li>");
+      }
+    }
+  });
 
-    socket.on('updateUsers', function (data) {
-        if(data != null){
-            $('#onlineUserList').html("");
-            for(var key in data.usernames){
-                //add the user to the list of online users
-                $('#onlineUserList').append("<li>"+key+"</li>");
-            }
-        }
-    }); 
+  //load history
+  socket.on("loadHistory", function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var html =
+        "<div class='post-other'> <div class='post-inner'><b>" +
+        data[i].user +
+        "</b> " +
+        data[i].message +
+        "</div></div>";
+      $("#liveChat").append(html);
+    }
+  });
 
-    //load history
-    socket.on('loadHistory', function(data) {
-
-        for(var i=0; i<data.length; i++){
-            var html = "<div class='post-other'> <div class='post-inner'><b>"
-            + data[i].user + "</b> "+ data[i].message + "</div></div>";
-            $('#liveChat').append(html);
-        }
-    });
-
-    socket.on('update',function (data) {
-        console.log("updatechat called");
-        var postClass;
-        if(data.user === user){
-            postClass = "post-current";
-        } else {
-            postClass = "post-other";
-        }
-        var html = "<div class='" + postClass + "'> <div class='post-inner'><b>"
-        + data.user + "</b> " + data.message + "</div></div>";
-        console.log("Message " + html);
-        $('#liveChat').append(html);
-        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-    });
+  socket.on("update", function(data) {
+    console.log("updatechat called");
+    var postClass;
+    if (data.user === user) {
+      postClass = "post-current";
+    } else {
+      postClass = "post-other";
+    }
+    var html =
+      "<div class='" +
+      postClass +
+      "'> <div class='post-inner'><b>" +
+      data.user +
+      "</b> " +
+      data.message +
+      "</div></div>";
+    console.log("Message " + html);
+    $("#liveChat").append(html);
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+  });
 });
-function channelSearch()
-{
-var url = "/" +  document.getElementById('channel-search').value;
-location.href = url;
-return false;
+function channelSearch() {
+  var url = "/" + document.getElementById("channel-search").value;
+  location.href = url;
+  return false;
 }
--- public/scripts/googleAuthentication.js
+++ public/scripts/googleAuthentication.js
@@ -1,25 +1,28 @@
 function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    var socket = io.connect(document.location.origin);
-    socket.on('news', function (data) {
-         console.log(data);
-     });
-     
-    console.log('Full Name: ' + profile.getName());					
-    var id_token = googleUser.getAuthResponse().id_token;
-    sessionStorage.username = profile.getName();
-    userProf = profile.getName();
-    $("#usernameLabel").text("Welcome, " + profile.getName());
-    $('#loginGoogleAccountBar').addClass("hide");
-    $('#googleSignIn').attr("disabled", true);
-    $("#sendMessageBar").removeClass("hide");
-    if(userProf != "") {
-             var usr = userProf;
-             var safe = usr.replace(/&/g, '&amp;').replace(/</g, '&lt;')
-             .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-             sessionStorage.username = safe;
-             socket.emit('adduser', sessionStorage.username);
-             $('#online').append("<li>"+userProf+"</li>");             
-    }
- };
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  var socket = io.connect(document.location.origin);
+  socket.on("news", function(data) {
+    console.log(data);
+  });
+
+  console.log("Full Name: " + profile.getName());
+  var id_token = googleUser.getAuthResponse().id_token;
+  sessionStorage.username = profile.getName();
+  userProf = profile.getName();
+  $("#usernameLabel").text("Welcome, " + profile.getName());
+  $("#loginGoogleAccountBar").addClass("hide");
+  $("#googleSignIn").attr("disabled", true);
+  $("#sendMessageBar").removeClass("hide");
+  if (userProf != "") {
+    var usr = userProf;
+    var safe = usr
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;");
+    sessionStorage.username = safe;
+    socket.emit("adduser", sessionStorage.username);
+    $("#online").append("<li>" + userProf + "</li>");
+  }
+}
