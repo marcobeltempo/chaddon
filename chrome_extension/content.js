2055c186195633b75f967ec375325010f6b1859d
Michael <m.pierre@live.ca>
2018-02-04 20:42:21 -0500

    Localhost dev branch

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,11 +1,67 @@
 // content.js
-chrome.runtime.onMessage.addListener(
-    function getUrl (request, sender, sendResponse) {
-      if( request.message === "clicked_browser_action" ) {
-        var firstHref = $("a[href^='http']").eq(0).attr("href");
-  
-        console.log('The URL is: ', firstHref);
-        sendRespone= firstHref;
-      }
-    }
-  );
+
+//Create button on page
+var notify = document.createElement("button");
+document.body.appendChild(notify);
+notify.style.position = "fixed"; //position needs to be consistent
+notify.style.right = "10px";
+notify.style.top = "8px";
+notify.style.zIndex = "9999"; //I want it to overlap anything
+notify.innerText = "Chaddon";
+
+//div contains the chatroom
+var chatWindow = document.createElement("div");
+document.body.appendChild(chatWindow);
+document.body.insertBefore(chatWindow, notify.nextSibling);
+chatWindow.style.display = "none";
+chatWindow.style.position = "fixed";
+chatWindow.style.right = "10px";
+chatWindow.style.top = "40px";
+chatWindow.style.zIndex = "9999";
+
+//positioned in the chatroom window, the rest of the chat functionality from heroku
+var iframe = document.createElement("iframe");
+chatWindow.appendChild(iframe);
+iframe.src = "http://localhost:3000/" + extractHostname(document.URL);
+iframe.style.height = "560px";
+iframe.style.width = "800px";
+
+function extractHostname(url) {
+  var hostname;
+  //find & remove protocol (http, ftp, etc.) and get hostname
+
+  if (url.indexOf("://") > -1) {
+    hostname = url.split("/")[2];
+  } else {
+    hostname = url.split("/")[0];
+  }
+
+  //find & remove port number
+  hostname = hostname.split(":")[0];
+  //find & remove "?"
+  hostname = hostname.split("?")[0];
+
+  return hostname;
+}
+
+notify.addEventListener("click", function() {
+  if (chatWindow.style.display == "none") {
+    chatWindow.style.display = "block";
+  } else {
+    chatWindow.style.display = "none";
+  }
+});
+
+chrome.runtime.onMessage.addListener(function getUrl(
+  request,
+  sender,
+  sendResponse
+) {
+  if (request.message === "clicked_browser_action") {
+    var firstHref = $("a[href^='http']")
+      .eq(0)
+      .attr("href");
+    console.log("The URL is: ", firstHref);
+    sendRespone = firstHref;
+  }
+});
--- public/scripts/googleAuthentication.js
+++ public/scripts/googleAuthentication.js
@@ -1,25 +1,38 @@
 function onSignIn(googleUser) {
+    console.info("google login");
     // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
     var socket = io.connect(document.location.origin);
     socket.on('news', function (data) {
          console.log(data);
      });
-     
-    console.log('Full Name: ' + profile.getName());					
+    var messageBar = '<form class="navbar-form" onSubmit="return false;"><label id="usernameLabel" class="col-2 col-form-label">Welcome, <a class="brand"></a></label><input class="span8" type="text" id="message" onkeydown="enterSend()" placeholder="Be nice"/><!--<input class="btn btn-primary span2" OnClick="myFunction()" type="button" id="sendGoogleLogin" value="Login" />*/--><input class="btn btn-primary span2" onclick="sendMessage()" type="button" id="send" value="Send" />';
+    console.log('Full Name: ' + profile.getName());         
     var id_token = googleUser.getAuthResponse().id_token;
+    var sendMessage = document.getElementById("sendMessageBar");
+     
     sessionStorage.username = profile.getName();
+    id_token = id_token.substring(0,200);
+    socket.emit ('verifyUserGoogle', {username : profile.getName(), token : id_token});
+    socket.on ('verifySuccess', function (data) {
+        console.info("Username: " +data.username + " Token: " + sessionStorage.token);
+        sessionStorage.token = data.token;
+
+    });
+
     userProf = profile.getName();
-    $("#usernameLabel").text("Welcome, " + profile.getName());
-    $('#loginGoogleAccountBar').addClass("hide");
-    $('#googleSignIn').attr("disabled", true);
-    $("#sendMessageBar").removeClass("hide");
+    document.getElementById("overlay").style.display = "none";
+    sendMessage.innerHTML =messageBar ;
+    $("#usernameLabel").text("Welcome, " + userProf);
+
+    sendMessage.classList.remove("hide");
+
     if(userProf != "") {
              var usr = userProf;
              var safe = usr.replace(/&/g, '&amp;').replace(/</g, '&lt;')
              .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
              sessionStorage.username = safe;
-             socket.emit('adduser', sessionStorage.username);
-             $('#online').append("<li>"+userProf+"</li>");             
+            // socket.emit('adduser', {username : sessionStorage.username, token : sessionStorage.token });
+            // $('#onlineUserList').append("<li class='userOnline'>"+userProf+"</li>");             
     }
  };
