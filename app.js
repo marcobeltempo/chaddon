2e4e853c73e41498ec0bded685226a10336204c3
Marco Beltempo <marco.beltempo@gmail.com>
2018-02-16 21:11:50 -0500

    Upgrades to Express v.4.16.0 and seperate routing from app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
  * Module dependencies.
  */
 
-var express = require("express"),
-  routes = require("./routes"),
-  io = require("socket.io");
-
-var app = (module.exports = express.createServer());
+var io = require("socket.io"),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorhandler = require('errorhandler');
+  var routes = require("./routes/index.js");
 
 const { Client } = require("pg");
 
@@ -21,63 +21,8 @@ client.connect();
 //global variable to store input parameter
 var params;
 
-// Configuration
+io = io.listen(routes.app);
 
-app.configure(function() {
-  app.set("views", "views");
-  app.set("view engine", "html");
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static("public"));
-});
-
-app.configure("development", function() {
-  app.use(
-    express.errorHandler({
-      dumpExceptions: true,
-      showStack: true
-    })
-  );
-});
-
-app.configure("production", function() {
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get("/", function(req, res) {
-  params = req.params.id;
-  res.sendfile("views/index.html", {
-    root: __dirname
-  });
-});
-
-app.get("/login", function(req, res) {
-  params = req.params.id;
-  res.sendfile("views/login.html", {
-    root: __dirname
-  });
-});
-
-app.get("/:id", function(req, res) {
-  params = req.params.id;
-  res.sendfile("views/chatroom.html", {
-    root: __dirname
-  });
-});
-
-io = io.listen(app);
-var port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    app.address().port,
-    app.settings.env
-  );
-});
 
 //usernames in room
 var localUser = {};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,45 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Configuration
+app.set("views", "views");
+app.set("view engine", "html");
+app.use(express.static("public"));
 /*
  * GET home page.
  */
+module.exports = {
 
-exports.index = function(req, res) {
+listen: app.listen(port, function() {
+  console.log(
+    "Express server listening on port %d in %s mode"
+  );
+}),
+
+index: function(req, res) {
   res.render("index", { title: "Express" });
+},
+
+// Routes
+home: app.get("/", function(req, res) {
+  params = req.params.id;
+  res.sendfile("views/index.html", {
+    root: "./"
+  });
+}),
+
+login: app.get("/login", function(req, res) {
+  params = req.params.id;
+  res.sendfile("views/login.html", {
+  root: "./"
+  });
+}),
+
+channelId: app.get("/:id", function(req, res) {
+  params = req.params.id;
+  res.sendfile("views/chatroom.html", {
+  root: "./"
+  });
+})
 };
