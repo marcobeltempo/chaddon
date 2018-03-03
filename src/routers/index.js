ad1f123d7adec85fc98efd2c659b591a5fad1ab7
Marco Beltempo <marco.beltempo@gmail.com>
2018-03-03 15:23:33 -0500

    Updated linting, Travis CI build tests, and pull request requirements (#41)
    * Updated eslint validation  rules
    
    * Updated linting rules
    
    * Fixed linting errors
    
    * Fixed linting errors
    
    * Added pull request guidelines
    
    * Fixed linebreak rule to check for unix  "LF" instead of Windows "CRLF"
    
    * Updated contributing guidelines

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,26 +1,26 @@
 module.exports = function(app) {
-  home: app.get("/", function(req, res) {
+  app.get("/", (req, res) => {
     params = req.params.id;
     res.sendFile("./src/views/index.html", {
       root: "./"
     });
   });
 
-  login: app.get("/login", function(req, res) {
+  app.get("/login", (req, res) => {
     params = req.params.id;
     res.sendFile("./src/views/login.html", {
       root: "./"
     });
   });
 
-  privacyPolicy: app.get("/privacy", function(req, res) {
+  app.get("/privacy", (req, res) => {
     params = req.params.id;
     res.sendFile("./src/views/privacy_policy.html", {
       root: "./"
     });
   });
 
-  channel: app.get("/:id", function(req, res) {
+  app.get("/:id", (req, res) => {
     params = req.params.id;
     res.sendFile("./src/views/chatroom.html", {
       root: "./"
