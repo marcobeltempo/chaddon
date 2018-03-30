de21092e74206f23a11351db07a8dafcf4821d87
Marco Beltempo <marco.beltempo@gmail.com>
2018-03-30 02:36:39 -0400

    Server will place clients whithin the "payloads.domain" value

--- src/chaddon.js
+++ src/chaddon.js
@@ -11,55 +11,58 @@ const envConfig = require(path.join(__dirname, "./config/envConfig.js"))(http);
 envConfig.startServer(http);
 
 // Routing
-console.log(path.join(__dirname, '../browser_extension'));
+app.use(express.static(path.join(__dirname, '../browser_extension')));
 
-app.use(express.static(path.join(__dirname, '/browser_extension')));
-console.log(path.join(__dirname, '../browser_extension'));
-// Chatroom
 var numUsers = 0;
 
 io.on('connection', function (socket) {
   var addedUser = false;
 
+  // when the client emits 'add user', this listens and executes
+  socket.on("add user", function (payload) {
+    if (addedUser) {
+      return;
+    };
+    console.log("Adding: ", payload.username);
+    console.log("Domain: ", payload.domain);
+    // we store the username in the socket session for this client
+    socket.username = payload.username;
+    socket.channel = payload.domain;
+    console.log("socket.channel : ", socket.channel);
+
+    socket.join(socket.channel);
+
+    numUsers += 1;
+    addedUser = true;
+    io.to(socket.channel).emit('login', {
+      numUsers: numUsers
+    });
+
+    socket.broadcast.to(socket.channel).emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
     // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
+    socket.broadcast.to(socket.channel).emit('new message', {
       username: socket.username,
       message: data
     });
   });
 
-  // when the client emits 'add user', this listens and executes
-  socket.on("add user", function (username) {
-    if (addedUser) {
-      return;
-    };
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    numUsers += 1;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
+    socket.broadcast.to(socket.channel).emit('typing', {
       username: socket.username
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
+    socket.broadcast.to(socket.channel).emit('stop typing', {
       username: socket.username
     });
   });
@@ -70,7 +73,7 @@ io.on('connection', function (socket) {
       numUsers -= 1;
 
       // echo globally that this client has left
-      socket.broadcast.emit('user left', {
+      socket.broadcast.to(socket.channel).emit('user left', {
         username: socket.username,
         numUsers: numUsers
       });
