9968cd4a4c2ff0c740b44caa84ed1e6a185a736e
Marco Beltempo <marco.beltempo@gmail.com>
2018-03-30 02:37:01 -0400

    Linting fixes

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -3,22 +3,22 @@ var dbClient = require(path.join(__dirname, "../db/dbConfig.js")).getClient;
 
 module.exports = function (http) {
 
-var module = {};
-module.dbClient;
-module.host = "https://localhost:",
-module.port = parseInt(process.env.PORT || 3000, 10),
-module.env = process.env.NODE_ENV || "development",
+  var module = {};
+  module.dbClient;
+  module.host = "https://localhost:",
+  module.port = parseInt(process.env.PORT || 3000, 10),
+  module.env = process.env.NODE_ENV || "development",
 
   module.forceSsl = function (req, res, next) {
-    if (req.headers["x-forwarded-proto"] !== "https") {
-      return res.redirect(["https://", req.get("Host"), req.url].join(""));
-    }
-    return next();
-  },
+      if (req.headers["x-forwarded-proto"] !== "https") {
+        return res.redirect(["https://", req.get("Host"), req.url].join(""));
+      }
+      return next();
+    },
 
   module.startServer = function (http) {
-    http.listen(module.port, function () {
-      console.info(`
+      http.listen(module.port, function () {
+        console.info(`
       __________________________________________________________
      |              Chaddon Express Server Started              |
      |----------------------------------------------------------|
@@ -27,14 +27,14 @@ module.env = process.env.NODE_ENV || "development",
      |   Link:  ${module.host}${module.port}                          |
      |__________________________________________________________|
      `);
-    });
+      });
 
-    //monitor idle db clients
-    dbClient(err => {
+      //monitor idle db clients
+      dbClient(err => {
         if (err) {
           console.error("Datbase client error: ", err);
         }
-    });
-  };
-return module;
+      });
+    };
+  return module;
 };
