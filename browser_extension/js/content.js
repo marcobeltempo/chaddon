c6152f4834a973d0ef2cd997a3e17920805d14c0
GeoffMcCollam <GeoffMcCollam>
2018-04-01 19:24:56 -0400

    added chaddon as a sidebar

---
+++ browser_extension/js/content.js
@@ -0,0 +1,55 @@
+
+var ChaddonOpen = false;
+var buttonspace = "20px";
+
+var chatbar = document.createElement('iframe');
+chatbar.id = "myChatbar";
+chatbar.src = chrome.extension.getURL("src/browser_action/browser_action.html");
+chatbar.style.cssText = "\
+	position:fixed;\
+	top:0px;\
+	right:"+buttonspace+";\
+	width:0%;\
+	height:100%;\
+	background:white;\
+	box-shadow:inset 0 0 1em black;\
+	z-index:999999;\
+";
+document.body.appendChild(chatbar);
+
+var chatbutton = document.createElement('div');
+chatbutton.style.cssText = "\
+	position:fixed;\
+	top:0px;\
+	right:0px;\
+	width:"+buttonspace+";\
+	height:100%;\
+	background:white;\
+	box-shadow:inset 0 0 1em black;\
+	z-index:999999;\
+";
+document.body.appendChild(chatbutton);
+
+		
+
+chatbutton.addEventListener("click", function() {
+  if (ChaddonOpen){
+	    chatbar.style.width = "0%";
+		ChaddonOpen = false;
+	
+  } else {
+        chatbar.style.width = "30%";
+		ChaddonOpen = true;
+  }
+});/*
+function toggleChaddon() {
+	if (ChaddonOpen){
+	    chatbar.style.width = "0%";
+		ChaddonOpen = false;
+	}
+	else{
+		chatbar.style.width = "30%";
+		ChaddonOpen = true;
+	}
+	
+}*/
