fe5c5e95b82186254ea80ee815755977bdcab2e8
Marco Beltempo <marco.beltempo@gmail.com>
2018-04-03 04:06:38 -0400

    Added default settings object + event listeners

--- browser_extension/src/options_custom/settings.js
+++ browser_extension/src/options_custom/settings.js
@@ -1,72 +1,66 @@
 window.addEvent("domready", function () {
     new FancySettings.initWithManifest(function (settings) {
 
-        var ManifestSettings = (function () {
-
-            var userSettings = [
-                {
-                    id: "general",
-                    _setNotificationChecked: settings.manifest.setNotification.element.checked,
-                    _setNotificationStyleColor: settings.manifest.setNotification.element.style.color,
-                    _setNotificationTime: settings.manifest.setNotificationTime,
-                    _setNotificationTimeDisabled: settings.manifest.setNotificationTime.element.disabled,
-                    _setNotificationTimeValue: settings.manifest.setNotificationTime.element.value,
-                },
-                {
-                    id: "appearance",
-                    theme: settings.manifest.themeDropdown,
-                    popupSize: settings.manifest.popupWindowSize,
+        var _setNotification = settings.manifest.setNotification.element;
+        var _setNotificationTime = settings.manifest.setNotificationTime.element;
+        var _popupSize = settings.manifest.popupWindowSize.element;
+        var _theme = settings.manifest.themeDropdown.element;
+        var _blackListDomains = settings.manifest.blacklistedListBox.element;
+        var chromeExtensionSettings = (function () {
+            var chaddonSettings = [];
+            chaddonSettings.push({
+                id: 'isInitialized',
+                settings: {
+                    isInitialized: true
                 }
-            ];
+            });
+            chaddonSettings.push({
+                id: 'general',
+                settings: {
+                    setNotification: false,
+                    setNotificationTime: 10,
+                }
+            });
+            chaddonSettings.push({
+                id: 'appearance',
+                settings: {
+                    theme: 'default_theme',
+                    popupSize: 'medium-736x414'
+                }
+            });
 
-            var userSettingsMap = new Map(userSettings.map(setting => [setting.id, setting]));
-       /*     return {
-                updateNotifications: userSettings.general._setNotificationChecked.onchange = function () {
-                    console.log("Checked? ", userSettings.general._setNotificationChecked)
-                    chrome.storage.sync.get('chaddonDefault', function (result) {
-                        console.log(" updateNotifications(): ", result);
-                        //{'chaddonDefault':['general']['setNotification']}
-                        chrome.storage.sync.set(result[chaddonDefault.general.setNotification], function (result) {
-                            console.log("About to set:", result);
+            return {
+                //set default settings
+                _setDefaultSettings() {
+                    //chrome.storage.sync.clear();
+                    chrome.storage.sync.set({
+                        chaddonSettings
+                    });
+                },
+                //listeners for setting changes
+                onchange() {
+                    _setNotification.onchange = function () {
+                            console.log("setNotification changed to ", _setNotification.value)
+                        },
+                        _setNotificationTime.onchange = function () {
+                            console.log("setNotificationTime changed to ", _setNotificationTime.value)
+                        },
+                        _popupSize.onchange = function () {
+                            console.log("popupSize changed to ", _popupSize.value)
+                        },
 
-                        })
-                    })
+                        _theme.onchange = function () {
+                            console.log("theme changed to ", _theme.value)
+                        }
+
                 }
             };
-*/
         })();
-      //  ManifestSettings.updateNotifications();
 
+        chromeExtensionSettings._setDefaultSettings();
+        chromeExtensionSettings.onchange();
+        chrome.storage.sync.get(null, function (result) {
+            console.log("Settings", result);
+        });
     });
 });
-/* 
-                    //set settings to deault if they haven't been initialized
-                    if (!chromeExtensionSettings.getInitialized()) {
-                        chromeExtensionSettings.setDefaultSettings();
-                    } else {
-                        chromeExtensionSettings.updateSetting("enableNotifications", manifestSettings.enableNotifications.checked);
-                        chromeExtensionSettings.updateSetting("setNotificationTime", manifestSettings.setNotificationTime.element.value);
-                    }
-                    console.log("Enabled? ", chromeExtensionSettings.getSettingValue("enableNotifications"));
-
-                    //set the display activation and setNotificationTime
-                    manifestSettings.enableNotifications.onchange = function () {
-                        chromeExtensionSettings.updateSetting("enableNotifications", manifestSettings.enableNotifications.checked);
-                        ghost(!manifestSettings.enableNotifications.checked);
-                    };
-
-                    manifestSettings.setNotificationTime.element.onchange = function () {
-                        chromeExtensionSettings.updateSetting("setNotificationTime", manifestSettings.setNotificationTime.element.value);
-                    };
-
-                    function ghost(isDeactivated) {
-                        manifestSettings.enableNotifications.style.color = isDeactivated ? 'graytext' : 'black'; // The label color.
-                        manifestSettings.setNotificationTime.element.disabled = isDeactivated; // The control manipulability.
-                    };
-
-                    if (!manifestSettings.enableNotifications.checked) {
-                        ghost(true);
-                    }
-
-                });
-            }); */
