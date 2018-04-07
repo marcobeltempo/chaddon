cbcabfb06c680d08a27b1572a2cdf8faac519be7
GeoffMcCollam <GeoffMcCollam>
2018-04-07 11:53:45 -0400

    channels, user list, channel list, channel switching

--- src/chaddon.js
+++ src/chaddon.js
@@ -16,6 +16,12 @@ app.use(express.static(path.join(__dirname, '../browser_extension')));
 
 var numUsers = 0;
 
+//usernames in room
+var localUser = {};
+
+//storing room specific history
+var history = {};
+
 io.on('connection', function (socket) {
   var addedUser = false;
 
@@ -24,6 +30,7 @@ io.on('connection', function (socket) {
     if (addedUser) {
       return;
     };
+	
     console.log("Adding: ", payload.username);
     console.log("Domain: ", payload.domain);
     // we store the username in the socket session for this client
@@ -31,27 +38,74 @@ io.on('connection', function (socket) {
     socket.channel = payload.domain;
     console.log("socket.channel : ", socket.channel);
 
+	//add user to channel
     socket.join(socket.channel);
+	
+	if (localUser["" + socket.channel] === undefined) {
+	  localUser["" + socket.channel] = {};
+	}
+	//if user isn't already in this chat somewhere else
+	if (localUser["" + socket.channel][socket.username] === undefined){
+	  localUser["" + socket.channel][socket.username] = 1;
+	  //add channel to user's other tabs
+	  io.in(socket.username).emit("updateRooms", {
+        room: socket.channel
+      });
+		
+      socket.broadcast.to(socket.channel).emit('user joined', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+	} 
+	else { //user is already in this chat
+      localUser["" + socket.channel][socket.username]++;
+	}
 
     numUsers += 1;
     addedUser = true;
     io.to(socket.channel).emit('login', {
       numUsers: numUsers
     });
-
-    socket.broadcast.to(socket.channel).emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
+	
+	//add user to list of users on the channel
+    io.in(socket.channel).emit("updateUsers", {
+	  usernames: localUser[socket.channel],
+	  room: socket.channel
+	});
+	
+    socket.join(socket.username);
+	//add user's current channels
+	for (var i in localUser) {
+	  if (localUser[i][socket.username] != undefined) {
+	    socket.emit("updateRooms", {
+		  room: i
+	    });
+	  }
+    }
+	
+	
+    if (history[socket.channel] !== undefined) {
+	  socket.emit("loadHistory", history[socket.channel]);
+    } else {
+	  history[socket.channel] = [];
+    }
   });
 
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
     // we tell the client to execute 'new message'
-    socket.to(socket.channel).emit('new message', {
+	var message = {
       username: socket.username,
-      message: data
-    });
+      message: data.message,
+	  room: data.room
+	};
+	
+	if (history[data.room] === undefined) {
+	  history[data.room] = [];
+	}
+	history[data.room].push(message);
+		  
+    socket.to(data.room).emit('new message', message);
   });
 
   // when the client emits 'typing', we broadcast it to others
@@ -68,16 +122,55 @@ io.on('connection', function (socket) {
     });
   });
 
+  socket.on("viewchannel", function (data) {
+    if (data.oldchannel != undefined) {
+      socket.leave(data.oldchannel);
+    }
+    socket.join(data.room);
+    socket.emit("updateUsers", {
+        usernames: localUser[data.room],
+        room: data.room
+    });
+    if (history[data.room] !== undefined) {
+      socket.emit("loadHistory", history[data.room]);
+    }
+  });
+  
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
     if (addedUser) {
       numUsers -= 1;
-
-      // echo globally that this client has left
-      socket.broadcast.to(socket.channel).emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
+	  if (localUser[socket.channel] != undefined){
+		//user is in this chat in multiple instances
+		if (localUser[socket.channel][socket.username] > 1){
+			localUser[socket.channel][socket.username]--;
+		} 
+		else if (delete localUser[socket.channel][socket.username]) {
+          if (!Object.keys(localUser[socket.channel]).length) { //last user left; room now empty
+            delete localUser[socket.channel];
+            delete history[socket.channel];
+          }
+		  
+		  io.sockets.in(socket.username).emit("updateRooms", {
+			disconnectFlag: true,
+			room: socket.channel
+		  });
+		  
+          // echo globally that this client has left
+          socket.broadcast.to(socket.channel).emit('user left', {
+            username: socket.username,
+            numUsers: numUsers
+          });
+        }
+	  }
+	  
+      io.in(socket.channel).emit("updateUsers", {
+	    usernames: localUser[socket.channel],
+	    room: socket.channel
+	  });
+	  
+      socket.leave(socket.username);
+      socket.leave(socket.channel);
     }
   });
 });
