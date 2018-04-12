fda3bde3f443f6ef0a6db6425384d5d69e54e802
Marco Beltempo <marco.beltempo@gmail.com>
2018-04-12 10:57:37 -0400

    Added https support

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -1,30 +1,30 @@
 var path = require('path');
 var dbClient = require(path.join(__dirname, "../db/dbConfig.js")).getClient;
 
-module.exports = function (http) {
+module.exports = function (https) {
 
   var module = {};
   module.dbClient;
-  module.host = "http://localhost:",
+  module.host = "https://localhost:",
   module.port = parseInt(process.env.PORT || 3000, 10),
   module.env = process.env.NODE_ENV || "development",
 
-  module.forceSsl = function (req, res, next) {
+  module.forceSSL = function (req, res, next) {
       if (req.headers["x-forwarded-proto"] !== "https") {
         return res.redirect(["https://", req.get("Host"), req.url].join(""));
       }
       return next();
     },
 
-  module.startServer = function (http) {
-      http.listen(module.port, function () {
+  module.startServer = function (https) {
+      https.listen(module.port, function () {
         console.info(`
       __________________________________________________________
      |              Chaddon Express Server Started              |
      |----------------------------------------------------------|
      |   Mode:  ${module.env}                                     |
      |   Port:  ${module.port}                                            |
-     |   Link:  ${module.host}${module.port}                           |
+     |   Link:  ${module.host}${module.port}                          |
      |__________________________________________________________|
      `);
       });
