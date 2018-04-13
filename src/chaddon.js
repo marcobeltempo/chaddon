ca10c5cd0565212c2a5383ca030e69b173517fe6
Marco Beltempo <marco.beltempo@gmail.com>
2018-04-13 14:47:58 -0400

    Aded the "pem" module to auto generate SSL leys for debugging

--- src/chaddon.js
+++ src/chaddon.js
@@ -7,35 +7,42 @@ var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var fs = require("fs");
 var path = require('path');
 
-require("dotenv").load(); // loadd .env file variables
+require('dotenv').load(); // loadd .env file variables
 require('./config/passport.js')(passport); // pass passport for configuration
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-// Read in our SSL test certs
-const httpsOptions = {
-  key: fs.readFileSync("./src/config/server.key"),
-  cert: fs.readFileSync("./src/config/server.csr")
-};
-
-//var http = require('http').createServer(app);
-var http = require('https').createServer(httpsOptions, app);
-var io = require('socket.io')(http);
-
 // Modules
-const envConfig = require(path.join(__dirname, "./config/envConfig.js"))(http);
-const dbClient = require(path.join(__dirname, "./db/dbConfig.js"));
+var serverConf = require(path.join(__dirname, './config/serverConf.js'));
+//const dbClient = require(path.join(__dirname, './db'))
+// Read in our SSL test certs
+var https = require('https');
+
+serverConf.getSSL(key => {
+  https
+    .createServer(
+      {
+        key: key.serviceKey,
+        cert: key.certificate
+      },
+      app
+    )
+    .listen(serverConf.port);
+  serverConf.serverMessage();
+});
+var io = require('socket.io')(https);
 
 // Express setup
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 // view engine + templating setup
 app.set('view engine', 'ejs');
@@ -48,11 +55,13 @@ mongoose.connect(process.env.MONGOOSE_URL, {
 });
 
 // Passport setup
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -60,43 +69,41 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 // Routing
 require('./routers/router.js')(app, passport); // load our routes and pass in our app with passport
 
-envConfig.startServer(http);
-
 var numUsers = 0;
 
-//usernames in room
+// usernames in room
 var localUser = {};
 
-//storing room specific history
+// storing room specific history
 var history = {};
 
 io.on('connection', function (socket) {
   var addedUser = false;
 
   // when the client emits 'add user', this listens and executes
-  socket.on("add user", function (payload) {
+  socket.on('add user', function (payload) {
     if (addedUser) {
       return;
-    };
+    }
 
-    console.log("Adding: ", payload.username);
-    console.log("Domain: ", payload.domain);
+    console.log('Adding: ', payload.username);
+    console.log('Domain: ', payload.domain);
     // we store the username in the socket session for this client
     socket.username = payload.username;
     socket.channel = payload.domain;
-    console.log("socket.channel : ", socket.channel);
+    console.log('socket.channel : ', socket.channel);
 
-    //add user to channel
+    // add user to channel
     socket.join(socket.channel);
 
-    if (localUser["" + socket.channel] === undefined) {
-      localUser["" + socket.channel] = {};
+    if (localUser['' + socket.channel] === undefined) {
+      localUser['' + socket.channel] = {};
     }
-    //if user isn't already in this chat somewhere else
-    if (localUser["" + socket.channel][socket.username] === undefined) {
-      localUser["" + socket.channel][socket.username] = 1;
-      //add channel to user's other tabs
-      io.in(socket.username).emit("updateRooms", {
+    // if user isn't already in this chat somewhere else
+    if (localUser['' + socket.channel][socket.username] === undefined) {
+      localUser['' + socket.channel][socket.username] = 1;
+      // add channel to user's other tabs
+      io.in(socket.username).emit('updateRooms', {
         room: socket.channel
       });
 
@@ -104,8 +111,10 @@ io.on('connection', function (socket) {
         username: socket.username,
         numUsers: numUsers
       });
-    } else { //user is already in this chat
-      localUser["" + socket.channel][socket.username] = localUser["" + socket.channel][socket.username] + 1;
+    } else {
+      // user is already in this chat
+      localUser['' + socket.channel][socket.username] =
+        localUser['' + socket.channel][socket.username] + 1;
     }
 
     numUsers += 1;
@@ -114,24 +123,24 @@ io.on('connection', function (socket) {
       numUsers: numUsers
     });
 
-    //add user to list of users on the channel
-    io.in(socket.channel).emit("updateUsers", {
+    // add user to list of users on the channel
+    io.in(socket.channel).emit('updateUsers', {
       usernames: localUser[socket.channel],
       room: socket.channel
     });
 
     socket.join(socket.username);
-    //add user's current channels
+    // add user's current channels
     for (var i in localUser) {
-      if (localUser[i][socket.username] != undefined) {
-        socket.emit("updateRooms", {
+      if (localUser[i][socket.username] !== undefined) {
+        socket.emit('updateRooms', {
           room: i
         });
       }
     }
 
     if (history[socket.channel] !== undefined) {
-      socket.emit("loadHistory", history[socket.channel]);
+      socket.emit('loadHistory', history[socket.channel]);
     } else {
       history[socket.channel] = [];
     }
@@ -168,17 +177,17 @@ io.on('connection', function (socket) {
     });
   });
 
-  socket.on("viewChannel", function (data) {
-    if (data.oldChannel != undefined) {
+  socket.on('viewChannel', function (data) {
+    if (data.oldChannel !== undefined) {
       socket.leave(data.oldChannel);
     }
     socket.join(data.room);
-    socket.emit("updateUsers", {
+    socket.emit('updateUsers', {
       usernames: localUser[data.room],
       room: data.room
     });
     if (history[data.room] !== undefined) {
-      socket.emit("loadHistory", history[data.room]);
+      socket.emit('loadHistory', history[data.room]);
     }
   });
 
@@ -186,17 +195,18 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function () {
     if (addedUser) {
       numUsers -= 1;
-      if (localUser[socket.channel] != undefined) {
-        //user is in this chat in multiple instances
+      if (localUser[socket.channel] !== undefined) {
+        // user is in this chat in multiple instances
         if (localUser[socket.channel][socket.username] > 1) {
           localUser[socket.channel][socket.username] -= 1;
         } else if (delete localUser[socket.channel][socket.username]) {
-          if (!Object.keys(localUser[socket.channel]).length) { //last user left; room now empty
+          if (!Object.keys(localUser[socket.channel]).length) {
+            // last user left; room now empty
             delete localUser[socket.channel];
             delete history[socket.channel];
           }
 
-          io.sockets.in(socket.username).emit("updateRooms", {
+          io.sockets.in(socket.username).emit('updateRooms', {
             disconnectFlag: true,
             room: socket.channel
           });
@@ -209,7 +219,7 @@ io.on('connection', function (socket) {
         }
       }
 
-      io.in(socket.channel).emit("updateUsers", {
+      io.in(socket.channel).emit('updateUsers', {
         usernames: localUser[socket.channel],
         room: socket.channel
       });
