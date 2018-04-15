f7a75619b6f982348a6beeb46b6084f9bdc834c8
Marco Beltempo <marco.beltempo@gmail.com>
2018-04-15 15:00:28 -0400

    Removed getSSL function

--- src/config/serverConf.js
+++ src/config/serverConf.js
@@ -1,11 +1,10 @@
 var path = require('path');
-var pem = require('pem');
 const dbClient = require(path.join(__dirname, '../db')).getClient;
 const debugServer= require('debug')('chaddon:server');
 
 module.exports = {
   dbCLient: dbClient,
-  host: 'https://localhost:',
+  host: 'http://localhost:',
   port: parseInt(process.env.PORT || 3000, 10),
   env: process.env.NODE_ENV || 'development',
 
@@ -15,30 +14,16 @@ module.exports = {
     }
     return next();
   },
-  getSSL: function (callback) {
-    pem.createCertificate(
-      {
-        days: 30,
-        selfSigned: true
-      },
-      function (err, keys) {
-        if (err) {
-          throw err;
-        }
-        return callback(keys);
-      }
-    );
-  },
   serverMessage: function () {
 
 const serverInfo = `
-      _______________________________________
-     |    Chaddon Express Server Started     |
-     |---------------------------------------|
-     |   Mode:  ${this.env}                  |
-     |   Port:  ${this.port}                         |
-     |   Link:  ${this.host}${this.port}       |
-     |_______________________________________|`;
+      __________________________________
+     |      Chaddon Express Server      |
+     |----------------------------------|
+     | Mode: ${this.env}                |
+     | Port: ${this.port}                       |
+     | Link: ${this.host}${this.port}      |
+     |__________________________________|`;
 
      debugServer('%s', serverInfo);
 
