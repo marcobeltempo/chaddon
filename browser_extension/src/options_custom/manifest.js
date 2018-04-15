635af7343847a13958f90705651d3aba0e855d91
Marco Beltempo <marco.beltempo@gmail.com>
2018-04-15 18:36:00 -0400

    Linting fixes

--- browser_extension/src/options_custom/manifest.js
+++ browser_extension/src/options_custom/manifest.js
@@ -1,53 +1,46 @@
-
-
 this.manifest = {
-    "name": "Chaddon Settings",
-    "icon": "../../icons/icon48.png",
-    "settings": [{
-            "tab": "General",
-            "group": "Notifications",
-            "name": "setNotification",
-			"id": "setNotification",
-            "type": "checkbox",
-            "label": "enable/disable"
-        },
-	 
-        {
-         "tab": "Blacklisted Domains",
-        "group": "Blacklisted",
-        "name": "domainToBlacklist",
-        "type": "text",
-        "label": "",
-        "text": "Add a domain to blacklist"
+  "name": "Chaddon Settings",
+  "icon": "../../icons/icon48.png",
+  "settings": [{
+      "tab": "General",
+      "group": "Notifications",
+      "name": "setNotification",
+      "id": "setNotification",
+      "type": "checkbox",
+      "label": "enable/disable"
     },
     {
-    "tab": "Blacklisted Domains",
-       "group": "Blacklisted",
-       "name": "addBlacklistDomainButton",
-       "type": "button",
-       "label": "",
-       "text": "Add"
-   },
-        {
-            "tab": "Blacklisted Domains",
-            "group": "Blacklisted",
-            "name": "blacklistedListBox",
-			"id": "blacklistedListBox",
-            "type": "listBox",
-            "label": "Chaddon will not enter a chatroom while browsing the domains listed below. (Refresh the page to see the updated blacklist)",
-            "options": [
-			
-				]
-                
-        },
-        {
-            "tab": "Blacklisted Domains",
-               "group": "Blacklisted",
-               "name": "deleteBlacklistDomainButton",
-               "type": "button",
-               "label": "",
-               "text": "Delete"
-           },
-    ]
-    
+      "tab": "Blacklisted Domains",
+      "group": "Blacklisted",
+      "name": "domainToBlacklist",
+      "type": "text",
+      "label": "",
+      "text": "Add a domain to blacklist"
+    },
+    {
+      "tab": "Blacklisted Domains",
+      "group": "Blacklisted",
+      "name": "addBlacklistDomainButton",
+      "type": "button",
+      "label": "",
+      "text": "Add"
+    },
+    {
+      "tab": "Blacklisted Domains",
+      "group": "Blacklisted",
+      "name": "blacklistedListBox",
+      "id": "blacklistedListBox",
+      "type": "listBox",
+      "label": "Chaddon will not enter a chatroom while browsing the domains listed below. (Refresh the page to see the updated blacklist)",
+      "options": []
+    },
+    {
+      "tab": "Blacklisted Domains",
+      "group": "Blacklisted",
+      "name": "deleteBlacklistDomainButton",
+      "type": "button",
+      "label": "",
+      "text": "Delete"
+    },
+  ]
 };
